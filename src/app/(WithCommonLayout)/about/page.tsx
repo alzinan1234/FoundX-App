e55ff516687f596568324b2feb90578b2e73e4bf
johir-dev <pappurohan1100@gmail.com>
2025-03-10 04:59:38 +0600

    first commit

---
+++ src/app/(WithCommonLayout)/about/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our about page. Here you can learn more about our mission and
+        values.
+      </p>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <h1>found x</h1>
+    </section>
+  );
+}
